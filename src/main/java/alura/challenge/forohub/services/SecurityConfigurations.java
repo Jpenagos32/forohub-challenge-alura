f7c7b2dd066219393cb941fd11829bb8034bbf29
jpenagos32 <jpenagosdev@gmail.com>
2025-01-15 20:31:45 -0500

    Agregar autenticacion con jwt

--- src/main/java/alura/challenge/forohub/services/SecurityConfigurations.java
+++ src/main/java/alura/challenge/forohub/services/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package alura.challenge.forohub.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+  @Autowired
+  private SecurityFilter securityFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,6 +27,13 @@ public class SecurityConfigurations {
       // desactivar la autenticacion con formulario de login (esto para API's) autenticación stateless
       .csrf(csrf -> csrf.disable())
       .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(requests -> requests
+        .requestMatchers(HttpMethod.POST, "/login")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+      )
+      .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
     ;
 
     return http.build();
--- src/main/java/alura/challenge/forohub/services/TokenService.java
+++ src/main/java/alura/challenge/forohub/services/TokenService.java
@@ -4,6 +4,9 @@ import alura.challenge.forohub.model.UsuarioModel;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -31,6 +34,29 @@ public class TokenService {
     } catch (JWTCreationException exception) {
       throw new RuntimeException();
     }
+  }
+
+  // necesario para verficar token
+  public String getSubject(String token) {
+    if (token == null){
+      throw new RuntimeException();
+    }
+    DecodedJWT verifier = null;
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+      verifier = JWT.require(algorithm)
+        .withIssuer("forohub")
+        .build()
+        .verify(token);
+      verifier.getSubject();
+
+    } catch (JWTVerificationException exception) {
+      System.out.println(exception.toString());
+    }
+    if (verifier.getSubject() == null){
+      throw new RuntimeException("Verifier inválido");
+    }
+    return verifier.getSubject();
   }
 
   private Instant generarFechaExpiracion(){
